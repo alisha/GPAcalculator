637ddbe2852e3a659af244caf8f9a8e70b8936d2
Alisha Ukani <alishaaukani@gmail.com>
2014-06-30 21:28:35 -0400

    Show unweighted GPA

--- calculateGPA.js
+++ calculateGPA.js
@@ -21,27 +21,30 @@ $(document).ready(function() {
 	$HonorsCredits = [4.7, 4.4, 4.0, 3.7, 3.4, 3.0, 2.7, 2.4, 2.0, 1.7, 1.4, 0];
 	$CPCredits = [4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0];
 	$FoundationsCredits = [3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.3, 0, 0, 0];
+	$unweightedScale = [4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0]
 
-	//Calculate the GPA
+	//Calculate the weighted and unweighted GPA
 	$('#findGPA').on('click', function() {
 		$numClasses = $('.className').size();
-		$credits = 0;
+		$weightedCredits = 0;
+		$unweightedCredits = 0;
 
 		//Count the total number of credits
 		for (var count = 0; count < $numClasses; count++) {
 			$grade = $('.grade').get(count).value.toUpperCase();
 			$type = $('.selectForm').get(count).value;
-			$credits += findCredits($type, $grade);
+			$weightedCredits += findWeightedCredits($type, $grade);
+			$unweightedCredits += findCredits($unweightedScale, $grade);
 		}
 
-		$gpa = ($credits/$numClasses).toFixed(2);
+		$weightedgpa = ($weightedCredits/$numClasses).toFixed(2);
+		$unweightedgpa = ($unweightedCredits/$numClasses).toFixed(2);
 
-		$('.results').html("<h1>Your GPA is: " + $gpa + "</h1>");
+		$('.results').html("<h1>Your weighted GPA is: " + $weightedgpa + "<br>Your unweighted GPA is: " + $unweightedgpa + "</h1>");
 		$('.results').scrollIntoView();
 	});
 
-	//Finds the number of credits for one class
-	function findCredits($type, $grade) {
+	function findWeightedCredits($type, $grade) {
 		//Determine which set of credits to use
 		$array = null;
 		if ($type == "AP") {
@@ -54,6 +57,11 @@ $(document).ready(function() {
 			$array = $FoundationsCredits;
 		}
 
+		return findCredits($array, $grade);
+	}
+
+	//Finds the number of credits for one class
+	function findCredits($array, $grade) {
 		//Find credits according to grade
 		$index = $letterGrades.indexOf($grade);
 		$classCredit = 0;
@@ -64,4 +72,5 @@ $(document).ready(function() {
 		}
 		return $classCredit;
 	}
+
 });
